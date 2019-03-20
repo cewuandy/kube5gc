1df722fbcd21e3c307a22677da3ad597b5d9fef4
root <root@sdn-k8s-A1.maas>
2019-03-20 08:15:22 +0000

    modify yaml

--- docker/setup-config.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-conf=$(ls /mnt/conf/)
-cat /mnt/conf/$conf > /root/free5gc/install/etc/free5gc/$conf
-freeDiameter=$(ls /mnt/freeDiameter)
-cat /mnt/freeDiameter/$freeDiameter > /root/free5gc/install/etc/free5gc/freeDiameter/$freeDiameter
